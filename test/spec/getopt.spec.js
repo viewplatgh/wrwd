1e5a6cea15d8057a19777c4b26600918e522f1cd
Rob L <viewpl@gmail.com>
2015-06-12 09:43:15 +1000

    Update bower dependencies and getopt.spec

---
+++ test/spec/getopt.spec.js
@@ -0,0 +1,13 @@
+/*jshint expr: true*/
+
+'use strict';
+
+var Getopt = require('../../client/scripts/getopt');
+
+describe('Give it some context', function() {
+
+  it('Should run a few assertions', function() {
+
+  });
+
+});
