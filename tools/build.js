f2c0648c7e02e66e3f31351eb6d79da7d318bdaa
Rob L <viewpl@gmail.com>
2015-06-24 04:37:01 +0000

    Update indentions

--- tools/build.js
+++ tools/build.js
@@ -4,14 +4,14 @@
     appDir: "../www",
     baseUrl: "js",
     paths: {
-    	jquery: "jquery-1.9.1",
-        jquery_ui: "ui/jquery-ui",
-    	jquery_layout: "jquery.layout-1.3.0",
-    	ace_theme_clouds: "ace/theme-clouds",
-    	ace_keybinding_vim: "ace/keybinding-vim",
-    	ace: "ace/ace",
-    	can: "amd/can",
-    	jstree: "jstree/jstree"
+      jquery: "jquery-1.9.1",
+      jquery_ui: "ui/jquery-ui",
+      jquery_layout: "jquery.layout-1.3.0",
+      ace_theme_clouds: "ace/theme-clouds",
+      ace_keybinding_vim: "ace/keybinding-vim",
+      ace: "ace/ace",
+      can: "amd/can",
+      jstree: "jstree/jstree"
     },
     dir: "../www-built",
     modules: [
