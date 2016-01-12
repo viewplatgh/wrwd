129576d79075e9ca6e2c0dabb38d50eaea2de4a1
Rob L <viewpl@gmail.com>
2016-01-12 15:58:04 +1100

    Add dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,23 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
+      'client/bower_components/jquery/dist/jquery.js',
+      'client/bower_components/angular/angular.js',
+      'client/bower_components/angular-resource/angular-resource.js',
+      'client/bower_components/angular-cookies/angular-cookies.js',
+      'client/bower_components/angular-sanitize/angular-sanitize.js',
+      'client/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'client/bower_components/lodash/dist/lodash.compat.js',
+      'client/bower_components/angular-socket-io/socket.js',
+      'client/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'client/bower_components/angular-validation-match/dist/angular-validation-match.min.js',
+      'client/bower_components/canjs/can.jquery.js',
+      'client/bower_components/jquery-ui/ui/jquery-ui.js',
+      'client/bower_components/jstree/dist/jstree.js',
+      'client/bower_components/sprintf/src/sprintf.js',
+      'client/bower_components/jquery.cookie/jquery.cookie.js',
+      'client/bower_components/angular-mocks/angular-mocks.js',
+      'client/bower_components/ace-builds/src-noconflict/ace.js',
       // endbower
       'node_modules/socket.io-client/socket.io.js',
       'client/app/app.js',
